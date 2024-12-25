ff2f9d28cd9fea12b014754e1531fa1081696770
RubenM19 <ruben.mondragon@unmsm.edu.pe>
2024-12-25 17:34:52 -0500

    Description update

--- src/components/sections/Profile.jsx
+++ src/components/sections/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = ({ language }) => {
       phone: "+51 947 561 740",
       github: "GitHub Profile",
       description:
-        "A proactive person, excellent team player, organized and responsible. I greatly enjoy learning new things. Graduated in Software Engineering, with experience in the development of dashboards in Power BI and automated flows in Power Automate to optimize processes. My interest in tools such as ETL, SQL, and Databricks is combined with collaboration and a hands-on approach, which allows me to work in a team to create efficient solutions in challenging environments.",
+        "Software Engineering graduate with a multidisciplinary focus on data analysis and process optimization. Recognized for my ability to solve complex problems, work effectively in teams, and adapt quickly to dynamic environments. I possess advanced skills in tools such as SQL, Power BI, and BigQuery, which I combine with an analytical mindset and effective communication to transform data into actionable strategies. Known for my initiative, leadership, and commitment to exceeding expectations, driving measurable results that directly impact business growth.",
     },
     es: {
       title: "Rubén Alberto Mondragón Zúñiga",
@@ -20,7 +20,7 @@ const Profile = ({ language }) => {
       phone: "+51 947 561 740",
       github: "Perfil de GitHub",
       description:
-        "Proactivo, bueno para trabajar en equipo, organizado y responsable. Disfruto mucho aprender cosas nuevas. Graduado en Ingeniería de Software, con experiencia en el desarrollo de tableros en Power BI y flujos automatizados en Power Automate para optimizar procesos. Mi interés en herramientas como ETL, SQL y Databricks se combina con la colaboración y un enfoque práctico, lo que me permite trabajar en equipo para crear soluciones eficientes en entornos desafiantes.",
+        "Egresado de Ingeniería de Software con un enfoque multidisciplinario en análisis de datos y optimización de procesos. Reconocido por mi capacidad para resolver problemas complejos, trabajar en equipo y adaptarme rápidamente a entornos dinámicos. Poseo habilidades avanzadas en herramientas como SQL, Power BI y BigQuery, que combino con una mentalidad analítica y una comunicación efectiva para transformar datos en estrategias accionables. Destaco por mi iniciativa, liderazgo y compromiso para superar expectativas, impulsando resultados medibles que impactan directamente en el crecimiento empresarial.",
     },
   };
 
